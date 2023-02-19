bc7b45f33f29e81cb4bcafca37d76afc6f38d2dc
Cesar-CARV <cesaruizvera@gmail.com>
2023-02-19 13:42:09 -0600

    subir la app

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = (props) => {
+    return(
+        <>
+            <h1 className="text-4xl font-black text-center md:w-2/3 mx-auto">
+                Seguimineto Pacientes <span className="text-indigo-600">Veterinaria</span>
+            </h1>
+        </>
+    )
+}
+
+export default Header;
